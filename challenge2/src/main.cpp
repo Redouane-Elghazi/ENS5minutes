06350bb50bb83909eb993f819a3112368d7c6501
Tashiqi <redouane.elghazi@gmail.com>
2017-04-01 15:34:22 +0200

    random? ??

--- challenge2/src/main.cpp
+++ challenge2/src/main.cpp
@@ -3,21 +3,23 @@
 #include <cmath>
 #include <queue>
 #include <set>
+#include <cstdlib>
+#include <ctime>
 #define ll long long
 #define valid(i, j) 0<=i and i<H and 0<=j and j<W
 using namespace std;
 const double eps=pow(10,-20);
-ll H, W, R;
+int H, W, R;
 
-ll calc_coverage(vector<string>& carte, vector<vector<set<pair<ll,ll>>>>& dependency, ll x, ll y){
-    ll res=0;
-    ll minl,maxr,i,j;
+int calc_coverage(vector<string>& carte, vector<vector<set<pair<int,int>>>>& dependency, int x, int y){
+    int res=0;
+    int minl,maxr,i,j;
     if(carte[x][y]=='#'){
         return 0;
     }
     //upper
     i=x+1;
-    minl=max(y-R, (ll)0);
+    minl=max(y-R, (int)0);
     maxr=min(W-1, y+R);
     while(i>0 and x-i<R){
         j=y;
@@ -56,7 +58,7 @@ ll calc_coverage(vector<string>& carte, vector<vector<set<pair<ll,ll>>>>& depend
     }
     //lower
     i=x-1;
-    minl=max(y-R, (ll)0);
+    minl=max(y-R, (int)0);
     maxr=min(W-1, y+R);
     while(i<H-1 and i-x<R){
         j=y;
@@ -96,14 +98,14 @@ ll calc_coverage(vector<string>& carte, vector<vector<set<pair<ll,ll>>>>& depend
     return res;
 }
 
-void update_dependency(vector<string>& carte, vector<vector<set<pair<ll,ll>>>>& dependency, ll x, ll y){
-    ll minl,maxr,i,j;
+void update_dependency(vector<string>& carte, vector<vector<set<pair<int,int>>>>& dependency, int x, int y){
+    int minl,maxr,i,j;
     if(carte[x][y]=='#'){
         return;
     }
     //upper
     i=x+1;
-    minl=max(y-R, (ll)0);
+    minl=max(y-R, (int)0);
     maxr=min(W-1, y+R);
     while(i>0 and x-i<R){
         j=y;
@@ -136,7 +138,7 @@ void update_dependency(vector<string>& carte, vector<vector<set<pair<ll,ll>>>>& 
     }
     //lower
     i=x-1;
-    minl=max(y-R, (ll)0);
+    minl=max(y-R, (int)0);
     maxr=min(W-1, y+R);
     while(i<H-1 and i-x<R){
         j=y;
@@ -170,9 +172,9 @@ void update_dependency(vector<string>& carte, vector<vector<set<pair<ll,ll>>>>& 
     return;
 }
 
-void placeRouter(vector<string>& carte, vector<vector<ll>>& closestB, set<pair<ll,ll>>& backbones, vector<vector<set<pair<ll,ll>>>>& dependency, ll r, ll c){
+void placeRouter(vector<string>& carte, vector<vector<int>>& closestB, set<pair<int,int>>& backbones, vector<vector<set<pair<int,int>>>>& dependency, int r, int c){
     update_dependency(carte, dependency, r, c);
-    ll x = r, y = c, i, j;
+    int x = r, y = c, i, j;
     while(closestB[x][y] != 0){
         backbones.emplace(x,y);
         i=x-1;
@@ -244,23 +246,23 @@ void placeRouter(vector<string>& carte, vector<vector<ll>>& closestB, set<pair<l
 
 int main()
 {
-    ll Pb, Pr, B, br, bc, r, c, i, j, x, y, m;
+    int Pb, Pr, B, br, bc, r, c, i, j, x, y, m;
     double optGain;
     cin >> H >> W >> R >> Pb >> Pr >> B >> br >> bc;
     vector<string> carte(H);
-    vector<vector<set<pair<ll,ll>>>> dependency (H, vector<set<pair<ll,ll>>> (W));
+    vector<vector<set<pair<int,int>>>> dependency (H, vector<set<pair<int,int>>> (W));
     for(i = 0; i<H; ++i)
         cin >> carte[i];
-    vector<vector<ll>> covered_area (H, vector<ll> (W));
+    vector<vector<int>> covered_area (H, vector<int> (W));
     vector<vector<double>> gain (H, vector<double> (W));
-    set<pair<ll,ll>> backbones, routers;
+    set<pair<int,int>> backbones, routers;
     backbones.emplace(br, bc);
-
+    srand (time(NULL));
 /// greedily placing routers (according to covered_area/cost
     while(true){
 /// finding the cost to link backbones
-        vector<vector<ll>> closestB (H, vector<ll> (W, -1));
-        queue<pair<ll,ll>> Q;
+        vector<vector<int>> closestB (H, vector<int> (W, -1));
+        queue<pair<int,int>> Q;
         for(auto& p:backbones){
             closestB[p.first][p.second] = 0;
             Q.emplace(p.first*W + p.second, 0);
@@ -329,7 +331,7 @@ int main()
 
 /// finding the maximum gain
         optGain = 0;
-        for(ll i = 0; i<H; ++i) for(ll j = 0; j<W; ++j) {
+        for(int i = rand()%R; i<H; i+=R) for(int j = rand()%R; j<W; j+=R) {
             covered_area[i][j] = calc_coverage(carte, dependency, i, j);
             gain[i][j] = covered_area[i][j]/(double)(closestB[i][j]*Pb+Pr);
             if(gain[i][j] > optGain and closestB[i][j]*Pb+Pr<=B){
@@ -348,9 +350,9 @@ int main()
 /// get a better solution by converging
     ///todo
 /// output
-    vector<vector<ll>> backcarte (H, vector<ll> (W, -1));
-    vector<pair<ll,ll>> backout;
-    queue<pair<ll,ll>> Q;
+    vector<vector<int>> backcarte (H, vector<int> (W, -1));
+    vector<pair<int,int>> backout;
+    queue<pair<int,int>> Q;
     Q.emplace(br,bc);
     for(auto& p:backbones){
         backcarte[p.first][p.second] = 0;
